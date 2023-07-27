dfe7447a5f75212fd6fc0620e5967ce176655165
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-07-28 00:29:26 +0300

    Added auto generated data

--- data-editor/src/mock-data.js
+++ data-editor/src/mock-data.js
@@ -1,6 +1,8 @@
+import { loremIpsum } from "lorem-ipsum";
+
 const mockData = Array.from({ length: 30 }, (v, i) => ({
     name: `foo${i + 1}`,
-    value: `bar${i + 1}`,
-  }));
+    value: loremIpsum({ count: Math.floor(Math.random() * 10) + 1, units: 'sentences' }),
+}));
 
 export { mockData };
